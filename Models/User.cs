a3cb8cae762dddb99f8e3ea7309ece4d72090870
Stanislav <91600823+Stanislav2177@users.noreply.github.com>
2024-06-04 23:10:58 +0300

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMaui.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using ProjectMaui.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectMaui.Services
+{
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Student>().Wait();
+            _database.CreateTableAsync<User>().Wait();
+        }
+
+        // Student operations
+        public Task<List<Student>> GetStudentsAsync() => _database.Table<Student>().ToListAsync();
+        public Task<int> SaveStudentAsync(Student student) => _database.InsertAsync(student);
+
+        // User operations
+        public Task<List<User>> GetUsersAsync() => _database.Table<User>().ToListAsync();
+        public Task<int> SaveUserAsync(User user) => _database.InsertAsync(user);
+    }
+}
